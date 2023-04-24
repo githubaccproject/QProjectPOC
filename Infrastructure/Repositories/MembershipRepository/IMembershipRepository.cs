5f18db26144b72a487a7cc157db8472acd2eb856
GithubAcc <psanny11@outlook.com>
2023-04-24 21:14:29 +0530

    Update IMembershipRepository.cs
    removed comments

--- Infrastructure/Repositories/MembershipRepository/IMembershipRepository.cs
+++ Infrastructure/Repositories/MembershipRepository/IMembershipRepository.cs
@@ -3,6 +3,6 @@ namespace Infrastructure.Repositories.MembershipRepository
 {
     public interface IMembershipRepository : IRepository<Membership>
     {
-        // Add any additional methods specific to Membership entity
+       
     }
 }
